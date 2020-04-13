4a0881d9ea44e76f5d724e30817c8eca65b7bf8e
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-04-13 19:23:21 -0400

    Faktoriyel

---
+++ src/InterviewQuestionss/Faktoriyel.java
@@ -0,0 +1,31 @@
+package InterviewQuestionss;
+
+import java.util.Scanner;
+
+public class Faktoriyel {
+
+	public static void faktoriyel() {
+		Scanner scan =new Scanner (System.in);//import java.util.Scanner;
+		System.out.println("number ");
+		
+		int num;
+		num=scan.nextInt();
+		int faktoriyel=1;
+		while (num>0) {
+			
+			faktoriyel=faktoriyel*num;
+			num--;
+		}
+		System.out.println("Faktoriyel " + faktoriyel);
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+
+		faktoriyel();
+		
+	}
+
+}
