8ad108f87a7c44d1216c48070aa000b9701fd7cd
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-05-19 22:07:19 -0400

    new

---
+++ src/Asel/IQ__1.java
@@ -0,0 +1,16 @@
+package Asel;
+
+public class IQ__1 {
+/*
+ * some questions from recent interviews. Please discuss them here or within your team
+:heavy_check_mark: What is mutable?
+:heavy_check_mark: What is static keyword in java and why do we use it?
+:heavy_check_mark: What is the difference between list and set?
+:heavy_check_mark: Give me an example of list or set from your framework?
+:heavy_check_mark: Can you write me a program for palindrome?
+:heavy_check_mark: Can you write me a program to reverse a string?
+:heavy_check_mark: Did you use super keyword in your project? What is it for?
+:heavy_check_mark: What is this keyword?
+ * 
+ */
+}
