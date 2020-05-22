7d035ced97ae0b23c1d1d75cd0bf252e09ec208a
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-05-21 20:10:36 -0400

    new

---
+++ src/Asel/IQ__2.java
@@ -0,0 +1,12 @@
+package Asel;
+/*
+ * @here some questions from todays/yesterdays interview at Freddie Mac, have fun :syntax:
+:question:Handling dynamic table 
+:question:Handling pop-up
+:question:Multiple Windows handling
+:question:What kind of Selenium locators do you know?
+:question:Difference between xpath and CSS
+ */
+public class IQ__2 {
+
+}
---
+++ src/WeeklyIQStudy/week__4.java
@@ -0,0 +1,20 @@
+package WeeklyIQStudy;
+/*
+ *National Student Clearing House Interview
+1)Tell me about your current project and your role and experience in the most recent job?
+2)What do you do when you don’t have enough time to test and rush testing in the last minute?
+3)What’s your approach and how do you develop test cases when the requirements are not very clear and not enough information on the story?
+4)Give me a specific example of a complex SQL query that you wrote from your project?  I gave max or second highest salary.
+5)Lets say we have a students table and there are some columns like student name, id San, country, city, address etc. Tell me the code to get how many students that is from each country in one column like there 1000 students from Canada, 50.000 from US etc. one by one all together.
+6)Did you use join command in your project? What is join?
+
+
+
+
+
+6:47
+arakadasa dun sorulan sorular 
+ */
+public class week__4 {
+
+}
