e172b40acd131b592711ab01c2f2d7b3ece00cad
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-05-23 14:13:17 -0400

    new

---
+++ src/WeeklyIQStudy/week__5.java
@@ -0,0 +1,19 @@
+package WeeklyIQStudy;
+/*
+ * 
+@Haciosmanoglu  Abstraction,Please give example and share your screen?
+@Gulen Interface, Please give example and share your screen?
+@Seyma Atasoy Atasoy String split,String trim(), String substring Please give example and share your screen?
+@Ayse Ozkan (online) Ozkan (online) String replace,String replaceAll, indexof() Please give example and share your screen?
+@Ali Kemal Tarlaci (online) wrapper classes,Autoboxing and Unboxing Please give example and share your screen?
+@Tugba Akca (online) Inheritance, Please give example and share your screen?
+@Turker Dogruer (online)  What are the differences between Heap and Stack Memory in Java?
+@Adile Karadeniz (online) Explain public static void main(String args[])?
+@Bayramgul Atageldiyeva Polymorphisms,Please give example and share your screen??
+@Servet AKGUC  Encapsulation example?
+Please share your screen.
+ * 
+ */
+public class week__5 {
+
+}
