52b793c81c232948ae1d83647e42822c520d3714
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-06-03 21:48:19 -0400

    new

---
+++ src/Group__IQ__Notes/Smoke_and_Regression_Test.java
@@ -0,0 +1,43 @@
+package Group__IQ__Notes;
+
+public class Smoke_and_Regression_Test {
+/*
+ * smoke test we are doing in the beginning of the test, in order to check main functionality. For example: we want to check adding Emp, but in order to check it we need to navigate to that page. So login in is smoke test. without login in we can not test another functions of the pg. in this test  we have 7-8 test cases -->@Test
+regression test : in this test we are testing all the functionality  (it is suite  test) of  an application. we are checking from the beginning to the end all the test cases.
+ 
+ Sanity testing is the surface level testing where QA engineer verifies that all the menus, functions, commands available in the product and project are working fine. This testing means to verify the newly added functionalities, bugs etc. are working fine. This is the first testing on the initial build.
+Sprint is a time how many weeks we need to work for specified tests. on whcich stories we will work are specified in sprint.when the current sprint is finished, it will be closed and the new one will be opened
+Product manager/scrum master/business analyst is giving us which test we must do. they are choosing which one is smoke/regression test
+
+
+Smoke Test
+Smoke test quickly scans the application through multiple high-level scenarios under 30 mins to make sure that the application is working fine (Environment is UP or Down).
+
+If something does not work, it sends out an email to everyone, letting them know that smoke test did not pass. This is mostly done through automated script.
+Automated Smoke test can be scheduled multiple times a day to give timely report to the team.
+
+Sanity Test
+The goal is to determine that the proposed functionality works roughly as expected. If sanity test fails, the build is rejected to save the time and costs involved in a more rigorous testing.
+Smoke Testing VS Sanity Test
+Smoke testing
+
+   Smoke testing is used to test all areas of the application without going into too deep.
+   A smoke test always uses an automated test or a written set of tests. It is always scripted.
+   Smoke testing is designed to include every part of the application in a not thorough or detailed way.
+   Smoke testing always ensures whether the most crucial functions of a program are working, but not bothering with finer details.
+   Sanity testing is a narrow test that focuses on one or a few areas of functionality, but not thoroughly or in-depth.
+   A sanity test is usually unscripted.
+Sanity testing is used to ensure that after a minor change a small part of the application is still working.
+
+Regression Testing
+Regression testing is a type of software testing that verifies that software previously developed and tested still performs correctly even after it was changed or interfaced with other software.
+
+if there is any new functionality is added or if there is any bug fixed or If there is any change in the code we have to make sure the new change made did not break existing functionality that was working fine.
+Regression testing suite will have a lot of test cases.
+Regression is scheduled ahead of time and it is team effort.
+Manual and Automation team will coordinate to run regression.
+ * 
+ * 
+ *  Regression Testing is a testing when the developer add new parts to application   then tester check aplication is working with new adding part smoothly or not.
+ */
+}
