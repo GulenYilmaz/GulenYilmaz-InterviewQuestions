45d9231f641f1c969754bc4e9ec9dbb39c398a61
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-06-13 23:58:16 -0400

    Group project US 9 and US10new

---
+++ src/Asel/IQ__3.java
@@ -0,0 +1,29 @@
+package Asel;
+/* Hello all, here are some questions from recent interviews, please practice them with your team members
+ProKarma technical interview questions
+Tell me about yourself
+Have you ever used testng
+testng annotations?
+What is difference between webdriver and remote webdriver?
+What type of waits do you use in your project? explain!
+Have you ever performed cross browser testing? what is cross browser testing?
+Have you performed parallel testing? what is parallel testing?
+What is POM? why using POM?
+tell me about cucumber and bdd approach?
+:thanks:
+2
+
+12:56
+My interview questions today (Cognizant -Florida, by phone)
+-	What is your position in your project, in your test team?
+-	What is your frame design pattern? (Note: questions are types by students, I am assuming for frame he is referring to framework)
+-	My frame dependencies
+-	How/Where are you using Junit in your project?
+-	How are you using JavaScript Executers?
+-	What is Java singleton pattern?
+-	If I know JavaScript or not?
+ * 
+ */
+public class IQ__3 {
+
+}
--- src/Mentor_A/week02.java
+++ src/Mentor_A/week02.java
@@ -1,5 +1,12 @@
 package Mentor_A;
-
+/*
+ * Good morning everyone, this Friday we will be going over:
+-- How to identify a prime number in Java.
+-- Collections
+-- Maps
+-- Final vs finally vs finalize (if we have time)
+Please revise above topics prior to our session. Please let me know if you have any questions
+ */
 public class week02 {
 
 }
