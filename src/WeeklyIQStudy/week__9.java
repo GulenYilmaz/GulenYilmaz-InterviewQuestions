81cb3f40f7725023f7ec92ff18a345ad5a74d997
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2020-06-26 11:48:30 -0400

    new

--- src/WeeklyIQStudy/week__9.java
+++ src/WeeklyIQStudy/week__9.java
@@ -2,9 +2,11 @@ package WeeklyIQStudy;
 /*
  * @ahmet What is the use of @Listener annotation in TestNG?
 @Ayse Ozkan (online) Ozkan (online) What is TestNG?What are the advantages of TestNG?
-@Ali Kemal Tarlaci (online) How to exclude a particular test method from a test case execution testng.xml file?
+@Ali Kemal Tarlaci (online) How to exclude a particular test method from a test 
+case execution testng.xml file?
 @Tugba Akca (online)How to create Group of Groups in TestNG.xml file?
-@Turker Dogruer (online)What is Maven?Why Maven? How it helps you developing your project effectively?
+@Turker Dogruer (online)What is Maven?Why Maven? How it helps you developing your 
+project effectively?
 @Bayramgul Atageldiyeva Difference between JUnit and TestNG
 @Kurt What is assertions in TestNG?
 @Gulen Where do you find your dependencies/libraries?What is .m2 folder?
@@ -12,5 +14,26 @@ package WeeklyIQStudy;
 @Adile Karadeniz (online)What are @Factory and @DataProvider annotation?
  */
 public class week__9 {
+/*
+What is the difference btw TESTNG and Junit
 
+
+
+TestNG vs Junit:                                                                                                                                                                                           TestNG supports testing in groups but it is not supported in JUnit.
+TestNG has a feature to configure dependency tests. Dependency test
+configuration for software web applications is not possible in JUnit.
+ Test prioritization, Parallel testing is possible in TestNG. 
+ It is not supported by
+JUnit.
+ TestNG compare to Junit provides additional annotations such as:
+ @BeforeSuite
+ @BeforeTest
+ @BeforeGroups
+ @AfterGroups
+ @Aftertest
+ @AfterSuite
+ * 
+ * 
+ * 
+ */
 }
